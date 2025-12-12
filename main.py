29bade53b30ef53980261df74665fa94116e86e8
Lokisonn <fenrir_lokisonn@mail.ru>
2025-12-12 13:01:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from application.salary import *
-from application.db.people import *
+import application.db.people
+from application.salary import calculate_salary
 from datetime import datetime
 
 if __name__ == '__main__':
+    print(datetime.now())
+    application.db.people.get_employees()
     calculate_salary()
-    get_employees()
